ddb46ccc0d3cc6d7a18ade4f414553869960d9b1
Your Name <thomas.w.yee@gmail.com>
2021-02-18 17:05:21 -0500

    DayLIstItem created

---
+++ src/components/DayListItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classnames from 'classnames';
+import 'components/DayListItem.scss';
+
+export default function DayListItem(props) {
+  const formatSpots = (spots) => {
+    return spots === 0
+      ? 'no spots remaining'
+      : spots === 1
+      ? `${spots} spot remaining`
+      : `${spots} spots remaining`;
+  };
+  const dayClass = classnames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0,
+  });
+  return (
+    <li onClick={props.setDay} className={dayClass}>
+      <h2 className='text--regular'>{props.name}</h2>
+      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
